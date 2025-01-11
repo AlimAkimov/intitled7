b2fd3bd403d3189b3d41269bc5f500a3ef9de247
Мавиле Сейтоплаева <mavile.seitoplaeva@gmail.com>
2025-01-11 20:34:25 +0300

    методы

--- src/Main.java
+++ src/Main.java
@@ -1,123 +1,82 @@
-import java.sql.SQLOutput;
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
-//        taks1();
+//        task1();
 //        task2();
-//        task3();
-//        task4();
-//        task5();
-//        task6();
-//        task7();
-//        task8();
-//        task9();
-        task10();
+        task3();
+
+
     }
-    public static void taks1() {
-//С помощью цикла for выведите в консоль все числа от 1 до 10.
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+
+    public static void task1() {
+        int year = 2024;
+        boolean isLeapYear = isLeapYear(year);
+        if (isLeapYear) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
     }
+    public static boolean isLeapYear(int year) {
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        return isLeapYear;
+    }
+
 
     public static void task2() {
-//        С помощью цикла for выведите в консоль все числа от 10 до 1.
-        for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+//        Напишите метод, куда подаются два параметра: тип операционной системы (0 — iOS, 1 — Android ) и год выпуска устройства.
+//        Если устройство старше текущего года, предложите ему установить облегченную версию.
+//        Текущий год можно получить таким способом:
+//        int currentYear = LocalDate.now().getYear();
+//        Или самим задать значение вручную — ввести в переменную числовое значение.
+//        В результате программа должна выводить в консоль сообщение, какую версию приложения (
+//        обычную или облегченную) и для какой ОС (Android или iOS) установить пользователю.
+
+        suggestAppVersion(1, 2025);
+
+    }
+    public static void suggestAppVersion(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        final int IOS = 0;
+        final int ANDROID = 1;
+        if (clientOS == IOS) {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Вот ссылка на стандартное приложение на iOS");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == ANDROID) {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Вот ссылка на стандартное приложение на Android");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Неизвестная операционная система");
         }
     }
+
 
     public static void task3() {
-//        Выведите в консоль все четные числа от 0 до 17.
-        for (int i = 0; i <= 17; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
-        }
+//        Ваша задача — доработать код, а именно написать метод, который на вход принимает дистанцию и возвращает
+//        итоговое количество дней доставки.
+        calculationOfDeliveryDays(40);
     }
 
-    public static void task4() {
-//        Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
-        for (int i = -10; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-
-    public static void task5() {
-//        Напишите программу, которая выводит в консоль все високосные года,
-//        начиная с 1904 года до 2096. В консоль результат должен выводиться в
-//        формате: «… год является високосным».
-        for (int leapYear = 1904; leapYear <= 2096; leapYear = leapYear+ 4) {
-            System.out.println(leapYear);
-        }
-    }
-
-    public static void task6() {
-//        Напишите программу, которая выводит в консоль последовательность чисел:
-//        7 14 21 28 35 42 49 56 63 70 77 84 91 98
-        for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
-        }
-    }
-
-    public static void task7() {
-//        Напишите программу, которая выводит в консоль последовательность чисел:
-//        1 2 4 8 16 32 64 128 256 512
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
-        }
-    }
-
-    public static void task8() {
-//        Посчитайте с помощью цикла for сумму годовых накоплений, если каждый месяц вы будете откладывать
-//        по 29 000 рублей «в банку».
-//        Выведите сумму накоплений за каждый месяц в консоль в формате: «Месяц …, сумма накоплений равна … рублей».
-        int saveMoney = 29000;
-        int total = 0;
-        for (int i = 1; i <= 12; i++) {
-            total = total + saveMoney;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+    public static void calculationOfDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            System.out.println("На доставку потребуется дней: '1'");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("На доставку потребуется дней: '2'");
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("На доставку потребуется дней: '3'");
+        } else if (deliveryDistance >= 100) {
+            System.out.println("Доставка не осуществляется");
         }
 
     }
-
-    public static void task9() {
-//        Перепишите решение задачи выше при условии, что деньги вы откладывать будете не «в банку»,
-//        а в банк под проценты — 12% годовых. Выведите сумму накоплений за каждый месяц в консоль
-//        в формате: «Месяц …, сумма накоплений равна … рублей».
-        int saveMoney = 29000;
-        int total = 0;
-        for (int i = 1; i <= 12; i++) {
-            total = total + total/100;
-            total = total + saveMoney;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
-
-        }
-    }
-
-    public static void task10() {
-//        Напишите программу, которая выводит в консоль таблицу умножения на 2:
-//        2*1=2
-//        2*2=4
-//        2*3=6
-//        2*4=8
-//        2*5=10
-//        2*6=12
-//        2*7=14
-//        2*8=16
-//        2*9=18
-//        2*10=20
-        int two = 2;
-        for (int i = 1; i <= 10; i++) {
-            int total = two * i;
-            System.out.println(two + "*" + i + "=" + total);
-        }
-    }
-
-
-
 
 
 }
-
